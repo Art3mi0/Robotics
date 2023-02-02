4d43d0450d7a4a19285106fd90fc157d1bab3d6a
Art3mi0 <temomez123@gmail.com>
2023-02-02 19:31:48 +0000

    added more code to script

--- robotics_lab1/scripts/control_node.py
+++ robotics_lab1/scripts/control_node.py
@@ -11,17 +11,31 @@ from robotics_lab1.msg import Turtlecontrol
 # for radians to degrees conversions
 import math
 
+def pose_callback(data):
+	global pos_msg
+	pos_msg.x = data.x 
+
+def control_callback(data):
+	global control_msg
+	control_msg.kp = data.kp
+	control_msg.xd = data.xd
+
 if __name__ == '__main__':
-	# declare a publisher to publish in the velocity command topic
-	cmd_pub = rospy.Publisher('/turtle1/control_params', Twist, queue_size = 10)
-	# initialize the node
-	rospy.init_node('control_node', anonymous = True)
 	# add a subscriber to it to read the position information
 	rospy.Subscriber('/turtle1/pose', Pose, pose_callback)
+	# add a subscriber to it to read the desired position information
+	rospy.Subscriber('/turtle1/control_params', Turtlecontrol, control_callback)
+	# declare a publisher to publish in the velocity command topic
+	cmd_pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size = 10)
+	# initialize the node
+	rospy.init_node('control_node', anonymous = True)
 	
 	loop_rate = rospy.Rate(10)
+	vel_cmd = Twist()
 
 	while not rospy.is_shutdown():
+		vel_cmd.linear.x = (control_msg.kp * (control_msg.xd - pos_msg.x))
+		cmd_pub.publisher(vel_cmd)
 		# wait for 0.1 seconds until the next loop and repeat
 		loop_rate.sleep()
 	
