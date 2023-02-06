80e38dfd2ef520136ca3fbe141939d485b5f4687
Art3mi0 <temomez123@gmail.com>
2023-02-06 19:39:15 +0000

    Worked on comments

--- robotics_lab1/scripts/control_node.py
+++ robotics_lab1/scripts/control_node.py
@@ -2,15 +2,14 @@
 
 # import ROS for developing the node
 import rospy
-# import geometry_msgs/Twist for control commands
+# import geometry_msgs/Twist for movement commands
 from geometry_msgs.msg import Twist
-# we are going to read turtlesim/Pose messages
+# importing turtlesim.msg pose for current turtle position data
 from turtlesim.msg import Pose
-#importing the new message from our package
+#importing the custom message for givng turtle location
 from robotics_lab1.msg import Turtlecontrol
-# for radians to degrees conversions
-import math
 
+# Initiating messages
 pos_msg = Pose()
 control_msg = Turtlecontrol()
 
@@ -24,20 +23,27 @@ def control_callback(data):
 	control_msg.xd = data.xd
 
 if __name__ == '__main__':
-	# add a subscriber to it to read the position information
+	# add a subscriber to read the current position information
 	rospy.Subscriber('/turtle1/pose', Pose, pose_callback)
-	# add a subscriber to it to read the desired position information
+	# add a subscriber to read the desired position information
 	rospy.Subscriber('/turtle1/control_params', Turtlecontrol, control_callback)
 	# declare a publisher to publish in the velocity command topic
 	cmd_pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size = 10)
 	# initialize the node
 	rospy.init_node('control_node', anonymous = True)
 	
+	# setting 10hz frequency for loop 
 	loop_rate = rospy.Rate(10)
+	# declarng a twist variable
 	vel_cmd = Twist()
+	# Setting desired location
+	control_msg.kp = 1
+	control_msg.xd = 8
 
 	while not rospy.is_shutdown():
+		# Performing formula to move towards desired location
 		vel_cmd.linear.x = (control_msg.kp * (control_msg.xd - pos_msg.x))
+		# Publish command
 		cmd_pub.publish(vel_cmd)
 		# wait for 0.1 seconds until the next loop and repeat
 		loop_rate.sleep()
