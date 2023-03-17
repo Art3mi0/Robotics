559c2fa1d109d0d954e8c2e8312c76b307881a22
Art3mi0 <temomez123@gmail.com>
2023-03-17 21:25:41 +0000

    Completed assignment

--- robotics_lab4/scripts/detect_ball.py
+++ robotics_lab4/scripts/detect_ball.py
@@ -9,9 +9,13 @@ img_received = False
 # define a 720x1280 3-channel image with all pixels equal to zero
 rgb_img = np.zeros((720, 1280, 3), dtype = "uint8")
 # define a lower range
-lower_yellow = np.array([100, 100, 1])
+lower_yellow = np.array([25, 10, 40])
 # define an upper range
-upper_yellow = np.array([255, 255, 100])
+upper_yellow = np.array([40, 255, 255])
+# defines an array similar to the image array
+img_array = np.zeros((720, 1280), dtype = "uint8")
+# Defines a rectangle used to focus on an area of the image
+rectangle = cv2.rectangle(img_array, (150, 150), (850, 550), (255, 255, 255), -1)
 
 
 # get the image message
@@ -27,7 +31,7 @@ def get_image(ros_img):
 if __name__ == '__main__':
 	# define the node and subcribers and publishers
 	rospy.init_node('ball_2d', anonymous = True)
-	# define a subscriber to ream images
+	# define a subscriber to read images
 	img_sub = rospy.Subscriber("/camera/color/image_raw", Image, get_image) 
 	# define a publisher to publish images
 	img_pub = rospy.Publisher('/ball_2d', Image, queue_size = 1)
@@ -38,10 +42,15 @@ if __name__ == '__main__':
 	while not rospy.is_shutdown():
 		# make sure we process if the camera has started streaming images
 		if img_received:
-			# flip the image up			
-			alt_img = cv2.inRange(rgb_img, lower_yellow, upper_yellow)
+			# Convert image to HSV space
+			hsv = cv2.cvtColor(rgb_img, cv2.COLOR_RGB2HSV)
+			# change rgb values to 0 if out of range or 255 if in range		
+			alt_img = cv2.inRange(hsv, lower_yellow, upper_yellow)
+			# Crops altered image to desired size
+			cropped_img = cv2.bitwise_and(rectangle, alt_img)
 			# convert it to ros msg and publish it
-			img_msg = CvBridge().cv2_to_imgmsg(alt_img, encoding="mono8")
+			img_msg = CvBridge().cv2_to_imgmsg(cropped_img, encoding="mono8")
+			
 			# publish the image
 			img_pub.publish(img_msg)
 		# pause until the next iteration			
