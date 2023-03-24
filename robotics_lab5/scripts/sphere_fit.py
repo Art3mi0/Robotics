0a7862cc893225cbd7dd37c3acd046834bf96452
Art3mi0 <temomez123@gmail.com>
2023-03-24 21:28:55 +0000

    Closer to solution

--- robotics_lab5/scripts/sphere_fit.py
+++ robotics_lab5/scripts/sphere_fit.py
@@ -6,25 +6,37 @@ from robot_vision_lectures.msg import XYZarray
 
 # Initialize messages
 sphere_params_msg = SphereParams()
-xyzarray_msg = XYZarray()
-
-def params_callback(data):
-	global sphere_params_msg
-	sphere_params_msg.points = data.points
+xyzarray_msg = []
+A = []
+B = []
 
 def array_callback(data):
 	global xyzarray_msg
-	xyzarray_msg.xc = data.xc
-	xyzarray_msg.yc = data.yc
-	xyzarray_msg.zc = data.zc
-	xyzarray_msg.radius = data.radius
+	global A
+	global B
+	A = []
+	A.append(data.points)
+
+def param_callback(data):
+	global sphere_params_msg
+	sphere_params_msg.xc = data.xc
+	sphere_params_msg.yc = data.yc
+	sphere_params_msg.zc = data.zc
+	sphere_params_msg.radius = data.radius
 
 if __name__ == '__main__':
 	# Intialize the node
 	rospy.init_node('sphere_fit', anonymous = True)
 	# define a subscriber to read ball detection output
-	rospy.Subscriber("/ball_2D", SphereParams, params_callback) 
+	rospy.Subscriber("/xyz_cropped_ball", XYZarray, array_callback) 
 	# define a publisher to publish estimated sphere parameters
-	param_pub = rospy.Publisher('/sphere_params', XYZarray, queue_size = 10)
+	#param_pub = rospy.Publisher('/sphere_params', SphereParams, queue_size = 10)
+	test_pub = rospy.Publisher('/XYZarray', XYZarray, queue_size = 10)
 	loop_rate = rospy.Rate(10)
-	estimate = XYZarray()
+	estimate = SphereParams()
+	
+	while not rospy.is_shutdown():
+		#test_pub.publish(xyzarray_msg)
+		print(A)
+
+		loop_rate.sleep()
